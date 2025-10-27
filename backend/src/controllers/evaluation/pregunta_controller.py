41b6f47581ac62234b2846187d82b4f1aeffb67d
Vannessa Junco <vjuncoh@itc.edu.co>
2025-10-26 19:11:51 -0500

    Controlador para agregar pregunta

---
+++ backend/src/controllers/evaluation/pregunta_controller.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from models.evaluation.pregunta import Pregunta
+from models.academic.tema import Tema
+from models.academic.rac import RAC
+from models.academic.parcial import Parcial
+import json
+
+class PreguntaController:
+    
+    @staticmethod
+    def agregar_pregunta(db: Session, datos_pregunta: dict):
+        
+        try: 
+            tema = db.query(Tema).filter(Tema.id_tema == datos_pregunta["id_tema"]).first()
+            rac = db.query(RAC).filter(RAC.id_rac == datos_pregunta["id_rac"]).first()  
+            parcial = db.query(Parcial).filter(Parcial.id_parcial == datos_pregunta["id_parcial"]).first()
+
+            if not tema:
+                    return {"error": "El tema no existe"}
+            if not rac:
+                    return {"error": "El RAC no existe"}
+            if not parcial:
+                    return {"error": "El parcial no existe"}
+            
+            nueva_pregunta = Pregunta(
+                    id_tema=datos_pregunta["id_tema"],
+                    id_rac=datos_pregunta["id_rac"],
+                    id_parcial=datos_pregunta["id_parcial"],
+                    enunciado=datos_pregunta["enunciado"],
+                    opciones=datos_pregunta["opciones"],  # ← Esto es el JSON
+                    respuesta_correcta=datos_pregunta["respuesta_correcta"],
+                    valor_base=datos_pregunta["valor_base"]
+                )
+            
+            db.add(nueva_pregunta)
+            db.commit()
+            db.refresh(nueva_pregunta)
+            return {
+                "mensaje": "Pregunta agregada exitosamente",
+                "id_pregunta": nueva_pregunta.id_pregunta
+            }
+        
+            
+        except Exception as e:
+            db.rollback()
+            return {"error": f"Error al crear pregunta: {str(e)}"}
+            
